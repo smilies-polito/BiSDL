de76e2f2d2eefa237e96e8537d428a7155645a89
bardini <roberta.bardini@polito.it>
2023-08-01 16:05:38 +0200

    giving structure to condition dependence, calibrating LacI administration

--- run_simulation.py
+++ run_simulation.py
@@ -13,21 +13,42 @@ n_steps = int(sys.argv[3])
 if example == "bacterial_consortium":
     test_module = Bacterialconsortium()
 
-if example == "rgb":
+elif example == "rgb":
     test_module = Rgb()
+else:
+    print("Insert valid example among the following: bacterial_consortium, rgb")
 
 output_path = os.path.join(".", "examples", example, "results", condition)
 s = Simulator(m=test_module, output_path=output_path, draw_nets=False)
 s.draw_nets(os.path.join(output_path, "../topology"))
 
-for _ in range(n_steps):
+if example == "bacterial_consortium":
+    if condition == "noLacI":
+        for _ in range(n_steps):
+            s.step()
 
-    marking = test_module.get_marking()
-    if _ == 50:
-        marking['controller_0_net']['Lac1_protein'].add([BlackToken()]*20)
-        s.set_initial_marking(marking)
+    elif condition == "lowLacI":
+        for _ in range(n_steps):
+            if _ % 5 == 0:
+                marking = test_module.get_marking()
+                marking['controller_0_net']['Lac1_protein'].add([BlackToken()] * 1)
+                s.step(init_marking=marking)
 
-    s.step()
+    elif condition == "highLacI":
+        for _ in range(n_steps):
+            if _ % 2 == 0:
+                marking = test_module.get_marking()
+                marking['controller_0_net']['Lac1_protein'].add([BlackToken()] * 1)
+                s.step(init_marking=marking)
+
+
+    else:
+        print("Insert valid condition among the following: noLacI, lowLacI, highLacI")
+
+elif example == "rgb":
+    if condition == "rgb":
+        for _ in range(n_steps):
+            s.step()
 
 s.make_charts()
 with open(os.path.join(output_path, "ascii_net_structure.txt"), 'w') as fp:
